258bc5c91d848f07b184d4d6f82b04f5d1fea0ea
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2022-12-26 13:07:43 -0300

    ...

---
+++ Pratica/pt005/javascriptu.js
@@ -0,0 +1,7 @@
+function contador(){
+    let nota1 = Number(window.document.getElementById('nota1'));
+    let nota2 = Number(window.document.getElementById('nota2'));
+    let div = window.document.getElementById('div-res');
+    let media = Number(nota1 + nota2 / 2);
+    
+}
