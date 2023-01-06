137399260fa4faf721fffcddb87806dfac010477
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2023-01-06 20:12:35 -0300

    Projeto login

--- Pratica/pt005/javascriptu.js
+++ Pratica/pt005/javascriptu.js
@@ -2,6 +2,11 @@ function contador(){
     let nota1 = Number(window.document.getElementById('nota1'));
     let nota2 = Number(window.document.getElementById('nota2'));
     let div = window.document.getElementById('div-res');
-    let media = Number(nota1 + nota2 / 2);
+    let media = Number(nota1+nota2/2);
     
+    if (media >= 60){
+        div.innerHTML = `Aprovado !`
+    }else{
+        div.innerHTML = `Reprovado`
+    }
 }
