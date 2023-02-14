b9746d9d6f4440a2b0c89d6f5692f188c2f9a243
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2023-02-14 19:53:50 -0300

    aa

--- Pratica/pt007/script.js
+++ Pratica/pt007/script.js
@@ -1,1 +1,26 @@
 //Javascript 
+
+let nameUser = document.getElementById('name')
+let password = document.getElementById('pass')
+let emailAcont = document.getElementById('email-accont')
+let idadeuser = document.getElementById('age')
+
+function Verification(nome,senha,email,idade){
+    nameUser = nome
+    password = senha
+    emailAcont = email
+    idadeuser = idade
+
+    const info = {
+        Nome: nome,
+        Idade: idade,
+        Email: email,
+        Senha: senha
+    }
+        //Dados legiveis
+        if(info === undefined){
+            String(nome,idade,email,senha)
+        }
+
+    console.log(info)
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # html-css
  Curso de HTML5 e CSS3 do Curso Em Vídeo
 
-<a href="https://alexandrefdb.github.io/html-css/Exercicios/ex001/index.html"> Executar o ex001.
+Seja bem-vindo ao meu README! Veja meus projetos e deixe seu feedback para que eu possa melhorar como um programador
