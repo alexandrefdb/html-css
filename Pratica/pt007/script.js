e307774098dee81cd6b1a968281485b4ca16182b
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2023-02-15 14:16:17 -0300

    cccc

--- Pratica/pt007/script.js
+++ Pratica/pt007/script.js
@@ -6,21 +6,26 @@ let emailAcont = document.getElementById('email-accont')
 let idadeuser = document.getElementById('age')
 
 function Verification(nome,senha,email,idade){
-    nameUser = nome
-    password = senha
-    emailAcont = email
-    idadeuser = idade
+    nome = nameUser.value
+    senha = password.value
+    email = emailAcont.value
+    idade = idadeuser.value
 
     const info = {
         Nome: nome,
         Idade: idade,
         Email: email,
         Senha: senha
-    }
-        //Dados legiveis
-        if(info === undefined){
-            String(nome,idade,email,senha)
-        }
+    }    
 
-    console.log(info)
+    if(info.value == undefined){
+        alert('Ocorreu um erro ! [404]')
+        console.log(info)
+    }else{
+        return Alert()
+    }
+}
+function Alert(){
+    window.alert(`Você foi cadastrado com sucesso!`)    
+    
 }
