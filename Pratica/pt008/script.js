9349e4264449a530626b5c498e6eb8a3d805437f
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2023-02-16 17:44:22 -0300

    Update script.js

--- Pratica/pt008/script.js
+++ Pratica/pt008/script.js
@@ -1,15 +1,24 @@
-let media = 79
+let media = 100
 
 if(media > 100){
     console.log('Não é possivel atribuir esse valor a Média.')
 }else{
-    if(media > 89){
-    console.log(`Parabéns, sua nota foi A`)
-    }else if(media >= 80 || media <= 89){
-        console.log(`Parabéns, sua nota foi B`)
-    }else{
-        if(media >= 70 || media <= 79){
-            console.log(`Parabéns, sua nota foi C`)
-        }
+    switch (media) {
+        case media >= 90:
+            console.log('Parabéns, sua nota foi A')
+            break;
+        case media >= 80 || media <= 89:
+            console.log('Parabéns, sua nota foi B')
+            break;
+        case  media >= 70 || media <= 79:
+            console.log('Sua nota foi C, foi uma boa nota')
+            break;
+        case media >= 60 || media <= 69:
+            console.log('Sua nota ficou na média, ela foi D!')
+            break;
+        default:
+            console.log('Sua nota foi F. Reprovado!')
+            break;
     }
+        
 }
