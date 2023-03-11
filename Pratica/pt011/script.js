fc3afa2cc3ccd62eb4de2c0690ba3506cdf6b549
Alexandre Ferreira de Brito <112659634+alexandrefdb@users.noreply.github.com>
2023-03-11 15:29:13 -0300

    :D

--- Pratica/pt011/script.js
+++ Pratica/pt011/script.js
@@ -1,2 +1,26 @@
-const biblioteca = ["Categoria", "Livro", "Autor"]
-console.log(biblioteca)
+const buttonOpenModal = document.querySelector('#openModal')
+const divVisible = document.querySelector('#Visivel')
+let ClickbuttonOpenModal = false
+buttonOpenModal.addEventListener('click', invisible)
+
+
+function invisible(){
+    divVisible.classList.toggle('invisible')
+    ClickbuttonOpenModal = true 
+}
+
+document.addEventListener('keydown', function(event){
+    const ESC = event.key === "Escape"
+
+    if(ESC){
+        if(ClickbuttonOpenModal === true){
+            divVisible.classList.add('invisible')
+            ClickbuttonOpenModal = false
+            console.log("Foi")
+        }else{
+           console.log('Num foi') 
+        }
+    }
+
+
+})
